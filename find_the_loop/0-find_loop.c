ab18f170ea6adc4824195a0a9e6736b84d12e017
Carlos <carl-frank7@hotmail.com>
2025-06-06 12:46:09 -0400

    loop 80 characters

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list using Floyd’s algorithm
+ * find_listint_loop - linked list using Floyd’s algorithm
  * @head: Pointer to the head of the list
  *
- * Return: Address of the node where the loop starts, or NULL if there is no loop
+ * Return: Address of the node where the loop starts,
+ * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
