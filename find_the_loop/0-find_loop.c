cb5c3bf37e7554b03655acde3d4bcfab2fdca790
Carlos <carl-frank7@hotmail.com>
2025-06-06 12:43:49 -0400

    betty

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,31 +1,32 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list using Floyd’s cycle algo.
- * @head: Pointer to the head of the list.
+ * find_listint_loop - Finds the loop in a linked list using Floyd’s algorithm
+ * @head: Pointer to the head of the list
  *
- * Return: Address of the node where the loop starts, or NULL if there is no loop.
+ * Return: Address of the node where the loop starts, or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);
-        }
-    }
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
